3cd1d85666012e553e99e107c340fee8f125117e
carson5326 <keilitz@umich.edu>
2023-08-09 13:35:25 -0400

    almost done with the javascript, need to update style

--- script.js
+++ script.js
@@ -45,12 +45,109 @@ function getResults(playerSelection, computerSelection) {
     else return "Error"
 }
 
-const button = document.querySelectorAll('.choiceButton');
-button.forEach((element) => {
-    element.addEventListener('click', () => output(element))
-})
 function output(e){
-    if (e.classList.contains('Rock')) console.log("Rock");
-    else if (e.classList.contains('Scissors')) console.log("Scissors");
-    else console.log("Paper");
+    if (e.classList.contains('Rock')) {
+        results.textContent = getResults(("Rock"), getComputerChoice());
+    }
+    else if (e.classList.contains('Scissors')) results.textContent = getResults(("Scissors"), getComputerChoice());
+    else results.textContent = getResults(("Paper"), getComputerChoice());
+    score.textContent = `${playerWins} Player ${computerWins} Computer`;
+    if(checkScore()) {
+        endGame();
+        restartButton();
+    }
+}
+
+function checkScore() {
+    
+    if (playerWins == 5) {
+        let childWinner = document.createElement("h1")
+        childWinner.classList.add("finalResults")
+        childWinner.textContent = "Winner"
+        finalResultsDiv.appendChild(childWinner)
+        return true;
+    }
+    else if (computerWins == 5) {
+        let childLoser = document.createElement("h1")
+        childLoser.classList.add("finalResults")
+        childLoser.textContent = "Loser"
+        finalResultsDiv.appendChild(childLoser)
+        return true
+    }
+    return false
+}
+function endGame() {
+    const remove = document.querySelectorAll(".remove");
+    let restart = document.querySelector(".restart")
+    playerWins = 0, computerWins = 0
+    remove.forEach((element) => {
+        element.remove();
+    });
+    restart.innerHTML = '<button class="startGame"> Restart </button>'
+}
+
+let score = document.querySelector(".score");
+let finalResultsDiv = document.querySelector(".finalResultsDiv");
+let button = document.querySelectorAll('.choiceButton');
+let results = document.querySelector(".results");
+//Returns Rock, Paper, and Scissors when clicking buttons
+addEventListeners()
+
+function restartButton() {
+    const startGame = document.querySelector(".startGame")
+    startGame.addEventListener('click', ()=> {
+        // remove results
+        addMainContent()
+        
+    })
+}
+
+function addMainContent() {
+    const childResults = document.createElement("div")
+    const add = document.querySelector(".add")
+    const childScore = document.createElement("div");
+    childScore.classList.add("score", "remove");
+    childResults.classList.add("results", "remove");
+    add.appendChild(childResults);
+    add.appendChild(childScore)
+    console.log(childResults)
+    
+    addButtonElements()
+    addEventListeners()
+    removeEndButtons()
+}
+//adds score, results, and button selectors to the new elements
+function addEventListeners(){
+    button = document.querySelectorAll('.choiceButton');
+    results = document.querySelector(".results");
+    score = document.querySelector(".score");
+    score.textContent = `${playerWins} Player ${computerWins} Computer`;
+    console.log(results)
+    button.forEach((element) => {
+        element.addEventListener('click', () => output(element))
+    })
+}
+
+function addButtonElements(){
+    const playerChoices = document.querySelector(".playerChoices")
+    const childRock = document.createElement('button')
+    childRock.classList.add("Rock", "choiceButton", "remove")
+    childRock.textContent = "Rock"
+    
+    const childScissors = document.createElement('button')
+    childScissors.classList.add("Scissors", "choiceButton", "remove")
+    childScissors.textContent = "Scissors"
+    
+    const childPaper = document.createElement('button')
+    childPaper.classList.add("Scissors", "choiceButton", "remove")
+    childPaper.textContent = "Paper"
+    playerChoices.appendChild(childRock)
+    playerChoices.appendChild(childScissors)
+    playerChoices.appendChild(childPaper)
+}
+function removeEndButtons() {
+    finalResults = document.querySelector('.finalResults')
+    restart = document.querySelector('.startGame')
+    finalResults.remove();
+    restart.remove()
 }
